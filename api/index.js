abeb4f349d44d532ea07eebbd778877a720f2a9f
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-19 10:25:28 -0500

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -12,10 +14,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
 
-app.use("/api/user", userRoutes);
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
