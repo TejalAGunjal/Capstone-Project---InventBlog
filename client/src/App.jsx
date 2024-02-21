17c7045f37cfd5aa6f7a2f5dca65874f6ae206ea
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-21 17:40:18 -0500

    add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
