729bfb61b73256c7a94f9624498551c81d1a8691
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-21 14:27:24 -0500

    sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -49,8 +49,7 @@ export default function SignUp() {
             Blog
           </Link>
           <p className="text-sm mt-5">
-            This is a demo project. You can sign up with your email and password
-            or with Google.
+            You can sign in with your email and password.
           </p>
           {/* right */}
           <div className="flex-1">
