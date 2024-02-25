a08a8f6206d6bd390b4fc991c580316cd2d5fa15
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-25 01:16:22 -0500

    add recent articles to postpage

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction/CallToAction";
 import CommentSection from "../components/CommentSection/CommentSection";
+import PostCard from "../components/PostCard/PostCard";
 // import PostCard from "../components/PostCard";
 
 export default function PostPage() {
@@ -35,6 +36,21 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
   if (loading)
     return (
       <div className="flex justify-center items-center min-h-screen">
@@ -76,10 +92,10 @@ export default function PostPage() {
 
       <div className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl mt-5">Recent articles</h1>
-        {/* <div className="flex flex-wrap gap-5 mt-5 justify-center">
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
           {recentPosts &&
             recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
-        </div> */}
+        </div>
       </div>
     </main>
   );
