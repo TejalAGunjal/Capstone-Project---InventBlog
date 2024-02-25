badb1d933681eca625e1a5bd252e0aa7902d8f11
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-25 17:25:26 -0500

    Dashboard Page updated

--- client/src/components/DashboardComp/DashboardComp.jsx
+++ client/src/components/DashboardComp/DashboardComp.jsx
@@ -77,13 +77,6 @@ export default function DashboardComp() {
             </div>
             <HiOutlineUserGroup className="bg-teal-600  text-white rounded-full text-5xl p-3 shadow-lg" />
           </div>
-          <div className="flex  gap-2 text-sm">
-            <span className="text-green-500 flex items-center">
-              <HiArrowNarrowUp />
-              {lastMonthUsers}
-            </span>
-            <div className="text-gray-500">Last month</div>
-          </div>
         </div>
         <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
           <div className="flex justify-between">
@@ -95,13 +88,6 @@ export default function DashboardComp() {
             </div>
             <HiAnnotation className="bg-indigo-600  text-white rounded-full text-5xl p-3 shadow-lg" />
           </div>
-          <div className="flex  gap-2 text-sm">
-            <span className="text-green-500 flex items-center">
-              {/* <HiArrowNarrowUp /> */}
-              {/* {lastMonthComments} */}
-            </span>
-            {/* <div className="text-gray-500">Last month</div> */}
-          </div>
         </div>
         <div className="flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md">
           <div className="flex justify-between">
@@ -111,20 +97,13 @@ export default function DashboardComp() {
             </div>
             <HiDocumentText className="bg-lime-600  text-white rounded-full text-5xl p-3 shadow-lg" />
           </div>
-          <div className="flex  gap-2 text-sm">
-            <span className="text-green-500 flex items-center">
-              <HiArrowNarrowUp />
-              {lastMonthPosts}
-            </span>
-            <div className="text-gray-500">Last month</div>
-          </div>
         </div>
       </div>
       <div className="flex flex-wrap gap-4 py-3 mx-auto justify-center">
         <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
           <div className="flex justify-between  p-3 text-sm font-semibold">
             <h1 className="text-center p-2">Recent users</h1>
-            <Button outline gradientDuoTone="purpleToPink">
+            <Button gradientDuoTone="pinkToOrange">
               <Link to={"/dashboard?tab=users"}>See all</Link>
             </Button>
           </div>
@@ -153,7 +132,7 @@ export default function DashboardComp() {
         <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
           <div className="flex justify-between  p-3 text-sm font-semibold">
             <h1 className="text-center p-2">Recent comments</h1>
-            <Button outline gradientDuoTone="purpleToPink">
+            <Button gradientDuoTone="pinkToOrange">
               <Link to={"/dashboard?tab=comments"}>See all</Link>
             </Button>
           </div>
@@ -178,7 +157,7 @@ export default function DashboardComp() {
         <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
           <div className="flex justify-between  p-3 text-sm font-semibold">
             <h1 className="text-center p-2">Recent posts</h1>
-            <Button outline gradientDuoTone="purpleToPink">
+            <Button gradientDuoTone="pinkToOrange">
               <Link to={"/dashboard?tab=posts"}>See all</Link>
             </Button>
           </div>
