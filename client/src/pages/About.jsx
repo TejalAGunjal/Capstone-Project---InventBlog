bb36e8e47c7fb1fd7170b7d9328884128a70b357
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-25 16:10:58 -0500

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,35 @@
-import React from "react";
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Invent Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Invent Blog! This blog is created by Tejal as a
+              CapeStone project to share thoughts and ideas with the world.
+              Tejal is a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+
+            <p>
+              Thank you for visiting our platform. We're thrilled to have you
+              here and excited to share our passion for [your niche/topic]. At
+              Invent Blog, we strive to create an engaging space where you can
+              find informative, entertaining, and thought-provoking content.
+            </p>
+
+            <p>
+              At Invent Blog, our mission is simple: to provide high-quality,
+              relevant content that enriches the lives of our readers. Whether
+              you're seeking practical advice, inspiration, or just a good read,
+              we aim to deliver content that resonates with you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
