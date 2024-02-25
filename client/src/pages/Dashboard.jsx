b7db2221c236e1bebf1626fb6c21ec55ea97ac6c
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-25 10:54:37 -0500

    add comment section to admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import DashSidebar from "../components/DashSidebar/DashSidebar";
 import DashProfile from "../components/DashProfile/DashProfile";
 import DashPosts from "../components/DashPosts/DashPosts";
 import DashUsers from "../components/DashUsers/DashUsers";
+import DashComments from "../components/DashComments/DashComments";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -30,6 +31,8 @@ export default function Dashboard() {
       {tab === "posts" && <DashPosts />}
       {/* users... */}
       {tab === "users" && <DashUsers />}
+      {/* Comments... */}
+      {tab === "comments" && <DashComments />}
     </div>
   );
 }
